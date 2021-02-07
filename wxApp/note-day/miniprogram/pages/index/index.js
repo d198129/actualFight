03a1084e947236d1ba8d804c51e9370909c2fe7d
d198129 <1612936195@qq.com>
2021-02-07 22:59:52 +0800

    每日一语首页

--- wxApp/note-day/miniprogram/pages/index/index.js
+++ wxApp/note-day/miniprogram/pages/index/index.js
@@ -1,15 +1,50 @@
 // miniprogram/pages/index/index.js
+import Dialog from '../../miniprogram_npm/@vant/weapp/dialog/dialog';
+const db = wx.cloud.database();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    userinfo: {}
+    userinfo: {},
+    height: 68,
+    daysay: []
   },
   
-  show() {
-    
+  onClose(event) {
+    console.log(event);
+    const { position, instance, name } = event.detail;
+    switch (position) {
+      case 'cell':
+        instance.close();
+        break;
+      case 'right':
+        Dialog.confirm({
+          message: '确定删除吗？'
+        }).then(() => {
+          wx.cloud.callFunction({
+            name: 'delsay',
+            data: {
+              name: name
+            }
+          }).then(res => {
+            console.log(res);
+            let daysay = this.data.daysay.filter(item => item._id !== name);
+            this.setData({
+              daysay
+            })
+          })
+          instance.close();
+        });
+        break;
+    }
+  },
+
+  add() {
+    wx.navigateTo({
+      url: '/pages/addsay/addsay'
+    });
   },
   /**
    * 生命周期函数--监听页面加载
@@ -22,13 +57,33 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    // let query = wx.createSelectorQuery();
+    // query.select('#swipe-cell').boundingClientRect(rect=>{
+    //   let height = rect.height;
+    //   this.setData({
+    //     height
+    //   })
+    // }).exec();
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    db.collection('say').orderBy('year','desc').orderBy('mouth','desc').orderBy('day','desc').orderBy('houre','desc').orderBy('min','desc').get().then(res => {
+      let say = res.data;
+      let daysay = [];
+      for(let i of say){
+        daysay.push({
+          _id: i._id,
+          say: i.say,
+          title: i.year + '-' + i.mouth + '-' + i.day + ' ' + i.houre + ':' + i.min
+        })
+      }
+      this.setData({
+        daysay
+      })
+    })
     const userinfo = wx.getStorageSync('userInfo');
     this.setData({
       userinfo
