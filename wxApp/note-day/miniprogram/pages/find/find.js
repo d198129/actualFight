a4d114e8bbd7707dc90997ecda7b17d05f07adf8
d198129 <1612936195@qq.com>
2021-02-08 11:43:17 +0800

    发现页，没全实现

--- wxApp/note-day/miniprogram/pages/find/find.js
+++ wxApp/note-day/miniprogram/pages/find/find.js
@@ -5,7 +5,40 @@ Page({
    * 页面的初始数据
    */
   data: {
-    userinfo: {}
+    userinfo: {},
+    fileList: [
+      {
+        url: 'https://img.yzcdn.cn/vant/leaf.jpg',
+        name: '图片1',
+      },
+      // Uploader 根据文件后缀来判断是否为图片文件
+      // 如果图片 URL 中不包含类型信息，可以添加 isImage 标记来声明
+      {
+        url: 'http://iph.href.lu/60x60?text=default',
+        name: '图片2',
+        isImage: true,
+        deletable: true,
+      },
+    ],
+  },
+
+  afterRead(event) {
+    const { file } = event.detail;
+    console.log(file);
+    // 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
+    wx.uploadFile({
+      url: 'https://example.weixin.qq.com/upload',
+      filePath: file.url,
+      name: 'file',
+      formData: { user: 'test' },
+      success(res) {
+        // 上传完成需要更新 fileList
+        console.log(123);
+        const { fileList = [] } = this.data;
+        fileList.push({ ...file, url: res.data });
+        this.setData({ fileList });
+      },
+    });
   },
 
   /**
